2c410c3793f35b3d9c429c1b7fd2274a62395a79
danielenapo <gnele2000@gmail.com>
2022-03-27 23:57:28 +0200

    working on/off button

---
+++ Assets/Prefabs/Button/OnOff.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems; //to access ar raycast
+using UnityEngine.UI;
+using TMPro; //for debugging text box
+
+public class OnOff : MonoBehaviour
+{
+    public GameObject buttonText;
+    private Vector2 touchPosition = default;
+    public Camera m_MainCamera;
+
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = touch.position;
+
+
+            //Checking to see if the position of the touch is over a UI object in case of UI overlay on screen.
+
+            //if (!touch.position.IsPointerOverUIObject())
+            // {
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = m_MainCamera.ScreenPointToRay(touchPosition);
+                RaycastHit hitObject;
+
+                if (Physics.Raycast(ray, out hitObject))
+                {
+                    //Do whatever you want to do with the hitObject, which in this case would be your, well, case. Identify it either through name or tag, for instance below.
+                    if (hitObject.collider.tag=="button") {
+                        if (hitObject.collider.GetComponentInChildren<TextMesh>().text == "OFF")
+                            hitObject.collider.GetComponentInChildren<TextMesh>().text = "ON";
+                        else
+                            hitObject.collider.GetComponentInChildren<TextMesh>().text = "OFF";
+                    }
+                }
+				else
+				{
+                    Debug.Log("nothing touched");
+				}
+            }
+            //}
+        }
+    }
+}
