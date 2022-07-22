881368adc9513e59fddcb0076094a4cb9c9f9568
danielenapo <gnele2000@gmail.com>
2022-07-22 16:59:44 +0200

    show data in actuators

--- Assets/Code/Proxy/GenericButton.cs
+++ Assets/Code/Proxy/GenericButton.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonDecorator : MonoBehaviour, Actuator
+public class GenericButton : MonoBehaviour, Actuator
 {
 	private CoapProxy proxy;
 	private string uri;
--- Assets/Code/Proxy/SliderDecorator.cs
+++ Assets/Code/Proxy/SliderDecorator.cs
@@ -4,10 +4,13 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Newtonsoft.Json;
+using TMPro;
 
 public class SliderDecorator : MonoBehaviour, IPointerUpHandler, Actuator
 {
 	public Text labelText;
+	public TextMeshProUGUI textValue;
 
 	private CoapProxy proxy;
 	private string uri;
@@ -17,6 +20,7 @@ public class SliderDecorator : MonoBehaviour, IPointerUpHandler, Actuator
 	{
 		this.proxy = proxy;
 		slider = this.gameObject.GetComponent<Slider>();
+		setValue(); //inizialmente imposta il valore prendendolo dal server
 	}
 
 	
@@ -26,7 +30,29 @@ public class SliderDecorator : MonoBehaviour, IPointerUpHandler, Actuator
 		proxy.post(uri, slider.value.ToString());
 	}
 
+	public void ValueChangeCheck()
+	{
+		textValue.text = slider.value.ToString();
+	}
 
+	public void setValue()
+	{
+		Dictionary<string, string> records = JsonParser.parse(proxy.get(uri))[0];
+		Debug.Log("SLIDER GET -> " + records.ToString());
+		try
+		{
+			if (records.ContainsKey("v"))
+			{
+				this.slider.value = float.Parse(records["v"]);
+				textValue.text = records["v"];
+			}
+		}
+		catch
+		{
+			this.slider.value = 0f;
+			textValue.text = "0";
+		}
+	}
 
 	public void setUri(string uri)
 	{
